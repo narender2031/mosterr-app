f7e4ac45b591712e496c93a8bf2e15a4f8f1a18e
Narender Singh <narender2031@gmail.com>
2020-04-01 18:33:52 +0530

    Updates

---
+++ precache-manifest.ad6bc93f24e2f8cab7ec37e84d905723.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf8fa055b35632653cda87c5390e659f",
+    "url": "/monsterr-app/index.html"
+  },
+  {
+    "revision": "35a59498dab5202c4025",
+    "url": "/monsterr-app/static/css/main.6b519aba.chunk.css"
+  },
+  {
+    "revision": "ea4f9f968c085804c354",
+    "url": "/monsterr-app/static/js/2.97804344.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsterr-app/static/js/2.97804344.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35a59498dab5202c4025",
+    "url": "/monsterr-app/static/js/main.a9429be8.chunk.js"
+  },
+  {
+    "revision": "102585bae7f33985441e",
+    "url": "/monsterr-app/static/js/runtime-main.1c547a4c.js"
+  }
+]);
